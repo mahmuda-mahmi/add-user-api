63b418f31eb0fe813521154acc80f44d426e5807
mahmuda-mahmi <mahmudapakter@gmail.com>
2024-06-02 16:09:17 +0600

    Co-authored-by: ifakhrul23@gmail.com <ifakhrul23@gmail.com>

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let newArr = []; 
+
 let showAll = false;
 
 const getData = async () => {
@@ -9,14 +10,15 @@ const getData = async () => {
        addUserData();
 }
 
-const addUserData = () => {
+const addUserData = (filterData = newArr) => {
        let tempArr;
        if(showAll){
-              tempArr = newArr;
+              tempArr = filterData;
        }
        else{
-              tempArr = newArr.slice(0,4);
+              tempArr = filterData.slice(0,4);
        }
+
        const userCard = document.getElementById('user-card');
        userCard.textContent = ' ';
        // tempArr.slice(0,4);
@@ -27,17 +29,53 @@ const addUserData = () => {
                                    <div>
                                           <img class="rounded-full mx-auto my-3" src="${element.picture.large}">
                                    </div>
-                                   <h3 class="font-bold text-center text-white mt-2">${element.name.first} ${element.name.last}, ${element.dob.age}</h3>
+                                   <h3 class="font-bold text-center text-white mt-2">${element.name.first} ${element.name.last}</h3>
+                                   <span class="my-2">${element.dob.age}, ${element.gender.toUpperCase()}</span>
+                                   <span class="my-2">${new Date(element.dob.date).toLocaleDateString()}</span>
                                    <span class="my-2">${element.location.state}, ${element.location.country}</span>
                                    <span class="mb-2">${element.location.timezone.description}</span>
                                    <button class="my-4 mx-auto">Resume</button>
        `;
               userCard.appendChild(div);
        });
+       // making show more button hide
+       const showMoreBtn = document.getElementById('show-more');
+       if(newArr.length>4 && !showAll){
+              showMoreBtn.removeAttribute('hidden');
+       }
+       else{
+              showMoreBtn.setAttribute('hidden', true);
+       }
 };
-
 
 const showMore = () => {
        showAll = true;
        addUserData();
+}
+
+const sortBtn = () => {
+       newArr.sort((a,b) => new Date(b.dob.date) - new Date(a.dob.date));
+       addUserData();
+}
+
+const searchInput = () => {
+       const inputText = document.getElementById("get-input");
+       const searchInputString = inputText.value.toLowerCase();
+       const searchInputValue = inputText.value;
+       inputText.value = ' ';
+       const filterData = newArr.filter(user => 
+              user.name.first.toLowerCase().includes(searchInputString) ||
+              user.name.last.toLowerCase().includes(searchInputString) ||
+              user.dob.age.toString().includes(searchInputValue) ||
+              user.location.state.toLowerCase().includes(searchInputString) ||
+              user.location.country.toLowerCase().includes(searchInputString) ||
+              user.gender.toLowerCase().includes(searchInputString)
+       );
+       addUserData(filterData);
+       if(filterData == []) {
+              !showAll;
+       }
+       else{
+              showAll = true;
+       }
 }
